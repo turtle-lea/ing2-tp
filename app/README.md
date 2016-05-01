49a3e7764a48f054121b2eadd10316094c9b3c66
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2016-04-30 22:23:32 -0300

    Modificado formato del main. Agregado README con las instrucciones para ejecutar

---
+++ app/README.md
@@ -0,0 +1,3 @@
+Run 'irb' in the console. This will open the interactive ruby shell.
+Run: load 'main.rb'
+Run: main()
--- app/main.rb
+++ app/main.rb
@@ -6,30 +6,33 @@ load 'init.rb'
 # @pivot = pivot 
 # @alapivot = alapivot 
 
-base_martin = Jugador.new("Martin")
-escolta_esteban = Jugador.new("Esteban")
-alero_leandro = Jugador.new("Leandro")
-pivot_matias = Jugador.new("Matias")
-ala_gaston = Jugador.new("Gaston")
+def main()
+  base_martin = Jugador.new("Martin")
+  escolta_esteban = Jugador.new("Esteban")
+  alero_leandro = Jugador.new("Leandro")
+  pivot_matias = Jugador.new("Matias")
+  ala_gaston = Jugador.new("Gaston")
 
-juan1 = Jugador.new("Juan1")
-juan2 = Jugador.new("Juan2")
-juan3 = Jugador.new("Juan3")
-juan4 = Jugador.new("Juan4")
-juan5 = Jugador.new("Juan5")
+  juan1 = Jugador.new("Juan1")
+  juan2 = Jugador.new("Juan2")
+  juan3 = Jugador.new("Juan3")
+  juan4 = Jugador.new("Juan4")
+  juan5 = Jugador.new("Juan5")
 
-equipo_rival = Equipo.new("All Blacks", juan1, juan2, juan3, juan4, juan5)
-los_pumas = Equipo.new("Los Pumas", base_martin, escolta_esteban, alero_leandro, pivot_matias, ala_gaston)
+  equipo_rival = Equipo.new("All Blacks", juan1, juan2, juan3, juan4, juan5)
+  los_pumas = Equipo.new("Los Pumas", base_martin, escolta_esteban, alero_leandro, pivot_matias, ala_gaston)
 
 
-libroDeJugadas = LibroDeJugadas.new(
-	[GeneradorDeJugadaOfensiva3PuntosKPases.new(CantidadDePases.new(2), FrecuenciaDeUso.new(10)),
-	 GeneradorDeJugadaOfensiva3PuntosKPases.new(CantidadDePases.new(5), FrecuenciaDeUso.new(90))],
-	 []);
+  libroDeJugadas = LibroDeJugadas.new(
+    [GeneradorDeJugadaOfensiva3PuntosKPases.new(CantidadDePases.new(2), FrecuenciaDeUso.new(10)),
+     GeneradorDeJugadaOfensiva3PuntosKPases.new(CantidadDePases.new(5), FrecuenciaDeUso.new(90))],
+     []);
 
-tecnico = Tecnico.new(los_pumas, libroDeJugadas);
+  tecnico = Tecnico.new(los_pumas, libroDeJugadas);
 
-tecnico.elegirJugadaOfensiva(1)
+  tecnico.elegirJugadaOfensiva(1)
+end
+
 #
 #equipo_rival.asignarOponente(los_pumas)
 #los_pumas.asignarOponente(equipo_rival)
